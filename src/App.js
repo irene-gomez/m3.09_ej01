4265021398dfe54d011d503df17cc5b5c12c705a
irenegf83 <irene@irenegf.com>
2019-05-16 22:58:48 +0200

    Add search number and filter if is even

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            inputValue: '',
+            isClicked: false
+        };
+        this.searchEven = this.searchEven.bind(this);
+        this.searchNumber = this.searchNumber.bind(this);
+    }
+
+    searchNumber(e) {
+        this.setState({
+            inputValue: parseInt(e.target.value)
+        });
+    }
+    searchEven() {
+        this.setState({
+            isClicked: !this.state.isClicked
+        });
+    }
+
+    render() {
+        const numbers = [1, 4, 6, 8, 45, 89];
+        const { inputValue, isClicked } = this.state;
+        console.log(isClicked);
+        return (
+            <div className="App">
+                <form>
+                    <input type="number" onChange={this.searchNumber} />
+                    <input type="checkbox" onClick={this.searchEven} /> Solo números pares
+                </form>
+                <ul className="list">
+                    {numbers
+                        .filter(num => num > inputValue)
+                        .filter(num => (isClicked ? num % 2 === 0 : true))
+                        .map((num, index) => (
+                            <li className="list__item" key={index}>{num}</li>
+                        ))}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
